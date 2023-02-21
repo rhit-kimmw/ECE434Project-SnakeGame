f1d8d7d4f6bde1784a72b498c1a67b2eae415e3f
ethanswallow <66641400+ethanswallow@users.noreply.github.com>
2023-02-20 19:23:29 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -6,10 +6,52 @@ if getpass.getuser() != 'root':
 import os
 import pygame
 import time
-
+import threading
+import Adafruit_BBIO.GPIO as GPIO
 import random
 from datetime import datetime
 from datetime import timedelta
+
+#Setup the Matrix
+import smbus
+bus = smbus.SMBus(2)
+matrix = 0x70
+indexHold = [0x80, 0x00, 0x00, 0x00,
+	 0x00, 0x00, 0x00, 0x00,
+	 0x00, 0x00, 0x00, 0x00,
+	 0x00, 0x00, 0x00, 0x00]
+
+bus.write_byte_data(matrix, 0x21, 0)
+bus.write_byte_data(matrix, 0x81, 0)
+bus.write_byte_data(matrix, 0xe7, 0)
+
+#Setup GPIO Buttons
+button1 = "P9_21"
+button2 = "P9_23"
+GPIO.setup(button1, GPIO.IN)
+GPIO.setup(button2, GPIO.IN)
+   
+one =  [0x02,0x00,0x02,0x00,0x02,0x00,0x02,0x00,
+         0x02,0x00,0x02,0x00,0x02,0x00,0x00,0x00]
+two = [0x0e,0x00,0x08,0x00,0x08,0x00,0x0e,0x00,
+       0x02,0x00,0x02,0x00,0x0e,0x00,0x00,0x00]
+three = [0x0e,0x00,0x02,0x00,0x02,0x00,0x0e,0x00,
+         0x02,0x00,0x02,0x00,0x0e,0x00,0x00,0x00]
+four = [0x02,0x00,0x02,0x00,0x02,0x00,0x0e,0x00,
+        0x0a,0x00,0x0a,0x00,0x0a,0x00,0x00,0x00]
+five = [0x0e,0x00,0x02,0x00,0x02,0x00,0x0e,0x00,
+       0x08,0x00,0x08,0x00,0x0e,0x00,0x00,0x00]
+six = [0x0e,0x00,0x0a,0x00,0x0a,0x00,0x0e,0x00,
+       0x08,0x00,0x08,0x00,0x0e,0x00,0x00,0x00]
+seven = [0x02,0x00,0x02,0x00,0x02,0x00,0x02,0x00,
+       0x0a,0x00,0x0a,0x00,0x0e,0x00,0x00,0x00]
+eight = [0x0e,0x00,0x0a,0x00,0x0a,0x00,0x0e,0x00,
+       0x0a,0x00,0x0a,0x00,0x0e,0x00,0x00,0x00]
+nine = [0x0e,0x00,0x02,0x00,0x02,0x00,0x0e,0x00,
+       0x0a,0x00,0x0a,0x00,0x0e,0x00,0x00,0x00]
+zero = [0x0e,0x00,0x0a,0x00,0x0a,0x00,0x0a,0x00,
+       0x0a,0x00,0x0a,0x00,0x0e,0x00,0x00,0x00]
+numbers = [zero,one,two,three,four,five,six,seven,eight,nine]
 
 class Game:
     screen = None
@@ -19,6 +61,9 @@ class Game:
     done = False
     clock = pygame.time.Clock()
     last_moved_time = datetime.now()
+    tick = 10
+    time = 0.1
+    score = 0
     
     def __init__(self): 
         "Ininitializes a new pygame screen using the framebuffer"
@@ -38,7 +83,37 @@ class Game:
         pygame.mouse.set_visible(False)
         # Initialise font support
         pygame.font.init()
+        
+    def timeincrease(self):
+        print("Time increase works")
+        Game.tick = Game.tick + 1
+        # self.Game.time = self.Game.time + 0.01
+   
+    def timedecrease(self):
+        print("Time decrease works")
+        Game.tick = Game.tick - 1
     
+    def increasescore(self):
+        self.displayNumber(self.score)
+    
+    def shift_to_tens_digit(self,ones_digit_matrix):
+        tens_digit_matrix = [0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
+        for i in range(len(ones_digit_matrix)):
+            if ones_digit_matrix[i] != 0:
+                tens_digit_matrix[i] = ones_digit_matrix[i] << 4
+        return tens_digit_matrix
+
+    def getGameMatrix(self,tens_digit, ones_digit):
+        game = [tens_digit[i] | ones_digit[i] for i in range(len(tens_digit))]
+        return game
+
+    def displayNumber(self, num):
+        tens_digit = self.shift_to_tens_digit(numbers[num // 10])
+        ones_digit = numbers[num%10]
+        game = self.getGameMatrix(tens_digit, ones_digit)
+        bus.write_i2c_block_data(matrix, 0, game)
+        
     def draw_block(self, screen, color, position):
         block = pygame.Rect((position[1] * 20, position[0] * 20),
                             (20, 20))
@@ -48,22 +123,55 @@ class Game:
         snake = Snake(self)
         apple = Apple(self)
         
+        # Encoder - input(direction for snake)
+        EncPath1 = '/sys/bus/counter/devices/counter2/count0'
+        f = open(EncPath1+'/ceiling', 'w')
+        f.write('1000000')
+        f.close()
+        f = open(EncPath1+'/count', 'w')
+        f.write('500000')
+        f.close()
+        f = open(EncPath1+'/enable', 'w')
+        f.write('1')
+        f.close()
+        f = open(EncPath1+'/count','r')
+        
+        oldDir = int(f.read())
+        #event for encoder
+        encoderEvent = pygame.USEREVENT + 1
+        pygame.time.set_timer(encoderEvent, 250)
+        
+        repeat = 0
         while not self.done:
-            self.clock.tick(10)
+            self.clock.tick(self.tick)
             self.screen.fill(self.WHITE)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.done = True 
-            if timedelta(seconds=0.1) <= datetime.now() - self.last_moved_time:
+                if event.type == encoderEvent:
+                    f.seek(0)
+                    newDir = int(f.read())
+                    
+                    right = (oldDir > newDir)
+                    left = (oldDir < newDir)
+                    
+                    oldDir = newDir
+                    snake.turn(right, left)
+                    
+            if timedelta(seconds=self.time) <= datetime.now() - self.last_moved_time:
                 snake.move()
                 self.last_moved_time = datetime.now()
                 
             if snake.positions[0] == apple.position:
                 snake.grow()
-                apple.position = (random.randint(0,19), random.randint(0,19))
+                self.increasescore()
+                apple.position = (random.randint(0,12), random.randint(0,16))
                 
             if snake.positions[0] in snake.positions[1:]:
+                self.done = True
+            
+            if snake.positions[0][0] > 12 or snake.positions[0][0] < 0 or snake.positions[0][1] > 16 or snake.positions[0][1] < 0:
                 self.done = True
                 
             snake.draw()
@@ -75,11 +183,23 @@ class Snake:
         self.positions = [(0,2),(0,1),(0,0)] 
         self.direction = ''
         self.Game = Game
+        self.directions = ['N','E','S','W']
+        self.direction_offset = 1
 
     def draw(self):
         for position in self.positions: 
             self.Game.draw_block(self.Game.screen, self.Game.GREEN, position)
-
+    
+    def turn(self, right, left):
+        if right:
+            self.direction_offset = (self.direction_offset + 1) % 4
+            self.direction = self.directions[self.direction_offset]
+            print('right', self.direction)
+        if left:
+            self.direction_offset = (self.direction_offset - 1) % 4
+            self.direction = self.directions[self.direction_offset]
+            print('left', self.direction)
+            
     def move(self):
         head_position = self.positions[0]
         y, x = head_position
@@ -93,6 +213,7 @@ class Snake:
             self.positions = [(y, x + 1)] + self.positions[:-1]
 
     def grow(self):
+        self.Game.score = self.Game.score + 1
         tail_position = self.positions[-1]
         y, x = tail_position
         if self.direction == 'N':
@@ -111,6 +232,25 @@ class Apple:
 
     def draw(self):
         self.Game.draw_block(self.Game.screen, self.Game.RED, self.position)
-        
-game = Game()
-game.runGame()
+  
+GPIO.add_event_detect(button1, GPIO.BOTH, callback=Game.timeincrease)
+GPIO.add_event_detect(button2, GPIO.BOTH, callback=Game.timedecrease)
+     
+ 
+button3 = "P9_26"
+GPIO.setup(button3, GPIO.IN)
+button4 = "P9_22"
+GPIO.setup(button4, GPIO.IN)
+start = GPIO.input(button3)
+done = GPIO.input(button4)
+while done == False:
+    start = GPIO.input(button3)
+    done = GPIO.input(button4)
+    if start:
+        print("Game Start")
+        bus.write_i2c_block_data(matrix, 0, zero)
+        game = Game()
+        game.runGame()
+    if done: 
+        print("Game Ended")
+        Game.done = True
