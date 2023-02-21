c05207c06176ceb1ba22b7869dc3151228680385
ethanswallow <66641400+ethanswallow@users.noreply.github.com>
2023-02-20 19:23:56 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 config-pin P8_11 eqep
 config-pin P8_12 eqep
-
+config-pin P9_19 i2c
+config-pin P9_20 i2c
 config-pin P9_21 gpio
+config-pin P9_22 gpio
 config-pin P9_23 gpio
 config-pin P9_26 gpio
